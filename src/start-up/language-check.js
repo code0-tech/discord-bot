9a4c08a0a0b1d9c4ad17f8706be8b9ce72d88a40
Nicusch <62447448+Nixkuchengifthub@users.noreply.github.com>
2024-05-04 17:44:11 +0200

    check lang.jsons if they match if not console.log missing keys etc

--- src/start-up/language-check.js
+++ src/start-up/language-check.js
@@ -2,14 +2,44 @@ const { loadJSONFilesFromFolder } = require('./../utils/json');
 const fs = require('fs').promises;
 const path = require('path');
 
+const checkLanguage = (exampleJson, langJson, langName) => {
+
+    for (const exampleCommandKey in exampleJson) {
+        if (langJson[exampleCommandKey]) {
+            const commandTexts = Object.keys(langJson[exampleCommandKey]);
+
+            exampleJson[exampleCommandKey].forEach(commandText => {
+                if (!commandTexts.includes(commandText)) {
+                    console.log(`In file ${langName}, text for /${exampleCommandKey} with text "${commandText}" is missing.`);
+                }
+            });
+        } else {
+            console.log(`In file ${langName}, text for /${exampleCommandKey} is missing.`);
+        }
+    }
+}
 
 
-const check = () => {
+const check = async () => {
     const folderPath = path.resolve(global.mainDir, 'languages');
 
-    const files = loadJSONFilesFromFolder(folderPath);
+    const files = await loadJSONFilesFromFolder(folderPath);
+
+    const compareJson = files['english'];
+
+    let exampleJson = [];
 
 
+    for (const key in compareJson) {
+        exampleJson[key] = Object.keys(compareJson[key]);
+    }
+
+
+    for (const key in files) {
+        if (key !== 'english') {
+            checkLanguage(exampleJson, files[key], key);
+        }
+    }
 }
 
 check();
