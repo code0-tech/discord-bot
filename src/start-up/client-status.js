dd35fab54c52a8bd0061151a1429d2af78e8df42
Nicusch <62447448+Nixkuchengifthub@users.noreply.github.com>
2024-05-09 21:33:40 +0200

    removed unused code

--- src/start-up/client-status.js
+++ src/start-up/client-status.js
@@ -1,31 +1,5 @@
 const config = require('./../../config.json');
 
-/* const setNextStatus = (name, status = 'online') => {
-    client.user.setPresence({ activities: [{ name }], status: status });
-}
-
-
-const start = () => {
-    const messages = config.status.messages;
-
-    const length = messages.length;
-    let index = 0;
-
-    setInterval(() => {
-        const message = messages[index];
-
-        setNextStatus(message.name, message.status);
-
-        if (index > length - 1) {
-            index = 0;
-        } else {
-            index = 0;
-        }
-    }, config.status.interval);
-
-} */
-
-
 const start = (client) => {
     const messages = config.status.messages;
     let index = 0;
