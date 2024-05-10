55de68610d1848540b13dbe5abbca999dbb91ea8
Nicusch <62447448+Nixkuchengifthub@users.noreply.github.com>
2024-05-10 13:32:45 +0200

    updated contributor command to new Embed system and added english

---
+++ src/commands/contributor.js
@@ -0,0 +1,26 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { Embed } = require('./../models/Embed');
+const config = require('./../../config.json');
+
+const data = new SlashCommandBuilder()
+    .setName('contributor')
+    .setDescription('Explore our Open Contributor guidelines and decide to submit your application.')
+
+const execute = async (interaction, client, guild, member, lang) => {
+    await interaction.deferReply({ ephemeral: true });
+
+    new Embed()
+        .setColor(config.embeds.colors.info)
+        .addInputs({
+            neededpr: config.commands.opencontributor.pr,
+            neededcommits: config.commands.opencontributor.commits
+        })
+        .addContext(lang, member, 'info')
+        .addCode0Footer()
+        .interactionResponse(interaction)
+};
+
+
+const componentIds = [];
+
+module.exports = { execute, componentIds, data };
