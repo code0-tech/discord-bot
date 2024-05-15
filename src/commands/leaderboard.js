b14aee11f893f39970308cd024ae5d5e0f234cfb
Nicusch <62447448+Nixkuchengifthub@users.noreply.github.com>
2024-05-15 22:51:12 +0200

    new desing for the table

--- src/commands/leaderboard.js
+++ src/commands/leaderboard.js
@@ -1,8 +1,11 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { MongoUser } = require('./../mongo/MongoUser');
 const { Mongo, ENUMS } = require('../models/Mongo');
+const { TableBuilder } = require('../models/table');
 const { getUser } = require('./../discord/user');
+
 const { Embed } = require('../models/Embed');
+
 const config = require('../../config.json');
 
 const MongoDb = new Mongo();
@@ -29,13 +32,40 @@ const listUser = async (limit = 10) => {
     return list;
 }
 
-const sendMessage = (interaction, member, lang, stringList) => {
+/* const sendMessage = (interaction, member, lang, stringList) => {
     new Embed()
         .setColor(config.embeds.colors.info)
         .addInputs({ stringlist: stringList })
         .addContext(lang, member, 'board')
         .interactionResponse(interaction);
+} */
+
+const sendMessage = (interaction, member, lang, data) => {
+
+    console.log(data)
+
+    const longestNameLength = Math.max(...data.map(entry => entry.name.length)) + 3;
+    const longestLevelLength = Math.max(...data.map(entry => entry.lvl.toString().length)) + 4;
+    const longestXPLength = Math.max(...data.map(entry => entry.xp.toString().length)) + 2;
+
+    const columns = [
+        { label: 'Name', field: 'name', width: longestNameLength },
+        { label: 'Lvl.', field: 'lvl', width: longestLevelLength },
+        { label: 'Xp', field: 'xp', width: longestXPLength }
+    ];
+
+    const tableBuilder = new TableBuilder(columns);
+
+    tableBuilder.addRows(...data);
+
+
+    new Embed()
+        .setColor(config.embeds.colors.info)
+        .addInputs({ stringlist: tableBuilder.build() })
+        .addContext(lang, member, 'board')
+        .interactionResponse(interaction);
 }
+
 
 
 const execute = async (interaction, client, guild, member, lang) => {
@@ -45,7 +75,7 @@ const execute = async (interaction, client, guild, member, lang) => {
 
     const userList = await listUser(limit);
 
-    let stringList = ``;
+    let data = [];
 
     const mongoUser = new MongoUser();
 
@@ -62,14 +92,14 @@ const execute = async (interaction, client, guild, member, lang) => {
 
         const username = leadboardMember.nickname == null ? leadboardMember.user.username : leadboardMember.nickname;
 
-        stringList += `\n${i + 1}. ${username} \`Level: ${level} [${xp}|${neededXp}]\``;
+        data.push({ name: username, lvl: level, xp: `[${xp}|${neededXp}]` });
 
         if ((i + 1) % 5 == 0) {
-            sendMessage(interaction, member, lang, stringList);
+            sendMessage(interaction, member, lang, data);
         }
     }
 
-    sendMessage(interaction, member, lang, stringList);
+    sendMessage(interaction, member, lang, data);
 
 };
 
