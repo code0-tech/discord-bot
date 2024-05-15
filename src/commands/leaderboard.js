912666dcbe701b70f69bbde431fb153b0a3a739b
Nicusch <62447448+Nixkuchengifthub@users.noreply.github.com>
2024-05-15 15:56:19 +0200

    remade style and added limit option for n'users in the embed to show from 1 to 100, maybe 100 will be changed to only 20 etc in the future depending on test's when having more that just 10 users total on the server like today 15.5.2024

--- src/commands/leaderboard.js
+++ src/commands/leaderboard.js
@@ -9,15 +9,21 @@ const MongoDb = new Mongo();
 const data = new SlashCommandBuilder()
     .setName('leaderboard')
     .setDescription('Check which user\'s are the top 10 best.')
+    .addIntegerOption(option =>
+        option
+            .setName('limit')
+            .setDescription('User to display in the ranking (default is 10)')
+            .setMinValue(1)
+            .setMaxValue(100)
+            .setRequired(false)
+    )
 
-const listUser = async (guild, limit = 10) => {
+const listUser = async (limit = 10) => {
 
     const list = await MongoDb.aggregate(ENUMS.DCB.USERS, [
         { $sort: { rawxp: -1 } },
         { $limit: limit }
     ])
-
-    // const newList = await list.map(async (item) => ({ ...item, username: await guild.members.fetch(item.id) }));
 
     return list;
 }
@@ -34,7 +40,9 @@ const sendMessage = (interaction, member, lang, stringList) => {
 const execute = async (interaction, client, guild, member, lang) => {
     await interaction.deferReply({ ephemeral: true });
 
-    const userList = await listUser(guild);
+    const limit = interaction.options._hoistedOptions.length !== 0 ? interaction.options._hoistedOptions[0].value : 10;
+
+    const userList = await listUser(limit);
 
     let stringList = ``;
 
@@ -49,9 +57,12 @@ const execute = async (interaction, client, guild, member, lang) => {
 
         const username = leadboardMember.nickname == null ? leadboardMember.user.username : leadboardMember.nickname;
 
-        stringList += `\n${user.id == member.user.id ? '[' : ''}${i + 1}.${user.id == member.user.id ? '](' + config.urls.code0 + ')' : ''} ${username} Level: ${level} [${xp}|${neededXp}] `;
+        // ${user.id == member.user.id ? '[' : ''}
+        // ${user.id == member.user.id ? '](' + config.urls.code0 + ')' : ''}
 
-        if (i == 0 || i == 5) {
+        stringList += `\n${i + 1}. ${username} \`Level: ${level} [${xp}|${neededXp}]\``;
+
+        if ((i + 1) % 5 == 0) {
             sendMessage(interaction, member, lang, stringList);
         }
     }
