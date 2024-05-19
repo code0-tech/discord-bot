638ec17c82a0f1f4528b80feea8159e9979aada6
Nicusch <62447448+Nixkuchengifthub@users.noreply.github.com>
2024-05-20 01:07:17 +0200

    todo

--- src/discord/user.js
+++ src/discord/user.js
@@ -2,6 +2,8 @@ const getUser = async (userid, guild) => {
     let member;
 
     try {
+        // create an check if user is in cache
+        
         member = await guild.members.fetch(userid);
     } catch (err) {
         member = null;
