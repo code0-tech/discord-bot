8573735f83ff1689e45ae414fb3f4c91da5ec12e
Nicusch <62447448+Nixkuchengifthub@users.noreply.github.com>
2024-05-20 00:51:08 +0200

    _appliaction init

---
+++ src/commands/_application.js
@@ -0,0 +1,17 @@
+const { ChannelType, PermissionFlagsBits, PermissionsBitField, ButtonBuilder, ButtonStyle, ActionRowBuilder, DiscordjsError, AttachmentBuilder } = require("discord.js");
+const { Embed, progressBar } = require('./../models/Embed');
+const config = require('./../../config.json');
+
+const data = null;
+
+
+const executeComponent = async (interaction, client, guild, buttonData, member, lang) => {
+    const defer = await interaction.deferReply({ ephemeral: true });
+
+}
+
+const componentIds = [
+    'test-id1'
+];
+
+module.exports = { executeComponent, componentIds, data };
