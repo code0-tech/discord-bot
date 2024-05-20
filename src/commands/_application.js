293200a5e1587da52eecaa51e2e18b2c16288791
Nicusch <62447448+Nixkuchengifthub@users.noreply.github.com>
2024-05-20 22:33:19 +0200

    todo & working on button updater

--- src/commands/_application.js
+++ src/commands/_application.js
@@ -22,22 +22,60 @@ const autoRun = async (client) => {
             message.delete();
         }
 
-        console.log(messages.get(messageId));
+        // console.log(messages.get(messageId));
 
     });
 
+
+
     // Go and check if message is up to date
 
+
+
+    if (messagesIds.length == 0) { // Testing purpose
+        const applyButton = new ButtonBuilder()
+            .setCustomId('application-apply-now')
+            .setLabel('Apply Now')
+            .setStyle(ButtonStyle.Primary);
+
+        const row = new ActionRowBuilder()
+            .addComponents(applyButton);
+
+
+        new Embed()
+            .setColor('#0099ff')
+            .setTitle('Test Apply Message')
+            .setDescription(progressBar(30, 100, true))
+            .responseToChannel(config.channels.application, client, [row])
+    }
+
 }
+
+/* 
+22:31:32 20.5.2024 => TypeError: Cannot read properties of null (reading 'name')
+    at button (E:\code\code0-discord-bot\src\interactions\emit-interactions.js:58:128)
+    at Client.<anonymous> (E:\code\code0-discord-bot\src\interactions\emit-interactions.js:75:13)   
+    at Client.emit (node:events:518:28)
+    at InteractionCreateAction.handle (E:\code\code0-discord-bot\node_modules\discord.js\src\client\actions\InteractionCreate.js:97:12)
+    at module.exports [as INTERACTION_CREATE] (E:\code\code0-discord-bot\node_modules\discord.js\src\client\websocket\handlers\INTERACTION_CREATE.js:4:36)
+    at WebSocketManager.handlePacket (E:\code\code0-discord-bot\node_modules\discord.js\src\client\websocket\WebSocketManager.js:355:31)
+    at WebSocketManager.<anonymous> (E:\code\code0-discord-bot\node_modules\discord.js\src\client\websocket\WebSocketManager.js:239:12)
+    at WebSocketManager.emit (E:\code\code0-discord-bot\node_modules\@vladfrangu\async_event_emitter\dist\index.cjs:282:31)
+    at WebSocketShard.<anonymous> (E:\code\code0-discord-bot\node_modules\@discordjs\ws\dist\index.js:1173:51)
+    at WebSocketShard.emit (E:\code\code0-discord-bot\node_modules\@vladfrangu\async_event_emitter\dist\index.cjs:282:31)
+
+*/
 
 
 const executeComponent = async (interaction, client, guild, buttonData, member, lang) => {
     const defer = await interaction.deferReply({ ephemeral: true });
 
+    console.log(buttonData)
+
 }
 
 const componentIds = [
-    'test-id1'
+    'application-apply-now'
 ];
 
 module.exports = { executeComponent, componentIds, data, autoRun };
--- src/commands/embed-design.js
+++ src/commands/embed-design.js
@@ -16,44 +16,29 @@ const data = new SlashCommandBuilder()
 const execute = async (interaction, client, guild, member, lang) => {
   await interaction.deferReply({ ephemeral: true });
 
-  const applyButton = new ButtonBuilder()
-    .setCustomId('x')
-    .setLabel('Apply Now')
-    .setStyle(ButtonStyle.Primary);
-
-  const row = new ActionRowBuilder()
-    .addComponents(applyButton);
 
 
-  new Embed()
-    .setColor('#0099ff')
-    .setTitle('pro')
-    .setDescription(progressBar(30, 100, true))
-    .interactionResponse(interaction, [row]);
-
-
-  /* 
-    await new Card()
-      .header({}, card => {
-        card.headerIcon({ value: 'X' }, card => { });
-        card.headerTitle({ value: `The reason was simple` }, card => { });
-        card.label({ value: 'info' }, card => { });
-      })
-      .body({}, card => {
-        card.subTitle({ value: `TTS...` }, card => { })
-        card.description({ value: `... didn't work with this and would cause problems` }, card => { });
-        card.description({ value: `for a small number of people,` }, card => { });
-        card.divider({}, card => { });
-        card.progressBar({ value: 50, label: true }, card => { });
-        card.description({ value: `especially because for some users the texts would be smaller than in their Dc settings.` }, card => { });
-        card.divider({}, card => { });
-        card.progressBar({ value: 10, label: false }, card => { });
-      })
-      .footer({}, card => {
-        card.footerIcon({ value: 'X' }, card => { });
-        card.footerTitle({ value: 'Code0 • But the code still exists' }, card => { });
-      })
-      .interactionResponse(interaction, [row]); */
+  await new Card()
+    .header({}, card => {
+      card.headerIcon({ value: 'X' }, card => { });
+      card.headerTitle({ value: `The reason was simple` }, card => { });
+      card.label({ value: 'info' }, card => { });
+    })
+    .body({}, card => {
+      card.subTitle({ value: `TTS...` }, card => { })
+      card.description({ value: `... didn't work with this and would cause problems` }, card => { });
+      card.description({ value: `for a small number of people,` }, card => { });
+      card.divider({}, card => { });
+      card.progressBar({ value: 50, label: true }, card => { });
+      card.description({ value: `especially because for some users the texts would be smaller than in their Dc settings.` }, card => { });
+      card.divider({}, card => { });
+      card.progressBar({ value: 10, label: false }, card => { });
+    })
+    .footer({}, card => {
+      card.footerIcon({ value: 'X' }, card => { });
+      card.footerTitle({ value: 'Code0 • But the code still exists' }, card => { });
+    })
+    .interactionResponse(interaction);
 
 }
 
