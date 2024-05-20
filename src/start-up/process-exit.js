46e3e6438c3f1474e8a9734c56f1e05ec9bab18e
Nicusch <62447448+Nixkuchengifthub@users.noreply.github.com>
2024-05-20 16:21:21 +0200

    removed //

--- src/start-up/process-exit.js
+++ src/start-up/process-exit.js
@@ -1,4 +1,3 @@
-// used for Nicusch local windows machine, for the simple reason: My console is fu**ed
 process.on('SIGINT', function () {
     console.log("\nShutting down from SIGINT (Ctrl-C)");
     process.exit(0);
