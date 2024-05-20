7f6beee65f8f7a6a37e9f066826974f8dc5a42c2
Nicusch <62447448+Nixkuchengifthub@users.noreply.github.com>
2024-05-20 02:21:18 +0200

    test

--- src/commands/embed-design.js
+++ src/commands/embed-design.js
@@ -15,35 +15,35 @@ const data = new SlashCommandBuilder()
 
 const execute = async (interaction, client, guild, member, lang) => {
   await interaction.deferReply({ ephemeral: true });
-  /* 
-    new Embed()
-      .setColor('#0099ff')
-      .setTitle('Leaderboard Test')
-      .setDescription(``)
-      .interactionResponse(interaction); */
 
-  /*  await new Card()
-     .header({}, card => {
-       card.headerIcon({ value: 'X' }, card => { });
-       card.headerTitle({ value: `The reason was simple` }, card => { });
-       card.label({ value: 'info' }, card => { });
-     })
-     .body({}, card => {
-       card.subTitle({ value: `TTS...` }, card => { })
-       card.description({ value: `... didn't work with this and would cause problems` }, card => { });
-       card.description({ value: `for a small number of people,` }, card => { });
-       card.divider({}, card => { });
-       card.progressBar({ value: 50, label: true }, card => { });
-       card.description({ value: `especially because for some users the texts would be smaller than in their Dc settings.` }, card => { });
-       card.divider({}, card => { });
-       card.progressBar({ value: 10, label: false }, card => { });
-     })
-     .footer({}, card => {
-       card.footerIcon({ value: 'X' }, card => { });
-       card.footerTitle({ value: 'Code0 • But the code still exists' }, card => { });
-     })
-     .interactionResponse(interaction);
-  */
+  new Embed()
+    .setColor('#0099ff')
+    .setTitle('pro')
+    .setDescription(progressBar(30, 100, true))
+    .interactionResponse(interaction);
+
+  /*await new Card()
+    .header({}, card => {
+      card.headerIcon({ value: 'X' }, card => { });
+      card.headerTitle({ value: `The reason was simple` }, card => { });
+      card.label({ value: 'info' }, card => { });
+    })
+    .body({}, card => {
+      card.subTitle({ value: `TTS...` }, card => { })
+      card.description({ value: `... didn't work with this and would cause problems` }, card => { });
+      card.description({ value: `for a small number of people,` }, card => { });
+      card.divider({}, card => { });
+      card.progressBar({ value: 50, label: true }, card => { });
+      card.description({ value: `especially because for some users the texts would be smaller than in their Dc settings.` }, card => { });
+      card.divider({}, card => { });
+      card.progressBar({ value: 10, label: false }, card => { });
+    })
+    .footer({}, card => {
+      card.footerIcon({ value: 'X' }, card => { });
+      card.footerTitle({ value: 'Code0 • But the code still exists' }, card => { });
+    })
+    .interactionResponse(interaction); */
+
 
 
   /* 
