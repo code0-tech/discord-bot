4dd29451ae8bf3b1dfb8c48190fd68897b396eff
Nicusch <62447448+Nixkuchengifthub@users.noreply.github.com>
2024-05-22 00:45:16 +0200

    code optim

--- src/interactions/load-interactions.js
+++ src/interactions/load-interactions.js
@@ -3,42 +3,40 @@ const path = require('path');
 const fs = require('fs');
 
 const load = (client) => {
-    const commandFiles = fs.readdirSync(path.join(global.mainDir, 'src', 'commands')).filter(file => file.endsWith('.js'));
+    const commandPath = path.join(global.mainDir, 'src', 'commands');
+    const commandFiles = fs.readdirSync(commandPath).filter(file => file.endsWith('.js'));
 
     client.commands = new Collection();
     client.components = new Collection();
 
-    commandFiles.forEach(commandFile => {
-        const command = require(path.join(global.mainDir, 'src', 'commands', commandFile));
+    for (const commandFile of commandFiles) {
+        const command = require(path.join(commandPath, commandFile));
+        const commandName = commandFile.split(".")[0];
 
-        command.fileName = commandFile.split(".")[0];
+        command.fileName = commandName;
 
         // Load Slash-Command
-        if (command.data !== null) {
+        if (command.data) {
             client.commands.set(command.data.name, command);
             console.log(`Loaded command: ${command.data.name}`);
-
         } else {
-            command.data = {
-                name: command.fileName
-            };
+            command.data = { name: commandName };
         }
 
         // Load Components
         if (command.executeComponent && command.componentIds) {
-            command.componentIds.forEach(buttonId => {
+            for (const buttonId of command.componentIds) {
                 client.components.set(buttonId, command);
-            });
-            console.log(`Loaded component: ${command.componentIds.length ? command.componentIds : '[]'} for: ${(command.data == null ? commandFile : command.data.name)}`);
+            }
+            console.log(`Loaded component: ${command.componentIds.length ? command.componentIds : '[]'} for: ${command.data.name}`);
         }
 
         // Start autoRun functions
         if (command.autoRun) {
-            command.autoRun(client)
+            command.autoRun(client);
             console.log(`Run autoRun function for ${commandFile}`);
         }
-
-    });
+    }
 
     require('./emit-interactions').setup(client);
 }
