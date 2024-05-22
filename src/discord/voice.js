05b376283c727673f3b8be0fd20e0f88335a1415
Nicusch <62447448+Nixkuchengifthub@users.noreply.github.com>
2024-05-22 21:10:13 +0200

    init

---
+++ src/discord/voice.js
@@ -0,0 +1,43 @@
+const checkState = async (oldState, newState) => {
+    let userId = newState.member ? newState.member.id : oldState.member.id;
+    let newUserChannel = newState.channel;
+    let oldUserChannel = oldState.channel;
+
+    if (oldUserChannel === null && newUserChannel !== null) {
+        return {
+            state: 'JOIN',
+            userid: userId,
+            newChannel: newUserChannel,
+            oldChannel: oldUserChannel
+        };
+    } else if (oldUserChannel !== null && newUserChannel === null) {
+        return {
+            state: 'LEAVE',
+            userid: userId,
+            newChannel: newUserChannel,
+            oldChannel: oldUserChannel
+        };
+    } else if (
+        oldUserChannel !== null &&
+        newUserChannel !== null &&
+        oldUserChannel.id != newUserChannel.id
+    ) {
+        return {
+            state: 'SWITCH',
+            userid: userId,
+            newChannel: newUserChannel,
+            oldChannel: oldUserChannel
+        };
+    } else {
+        // Optionally handle the case where no state change occurs
+        return {
+            state: 'NO_CHANGE',
+            userid: userId,
+            newChannel: newUserChannel,
+            oldChannel: oldUserChannel
+
+        };
+    }
+}
+
+module.exports = { checkState };
