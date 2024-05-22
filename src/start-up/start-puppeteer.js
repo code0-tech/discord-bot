42f96073ba10f5f9459f17886d8a9c01df2d3c5e
Nicusch <62447448+Nixkuchengifthub@users.noreply.github.com>
2024-05-23 00:01:17 +0200

    removed comments

--- src/start-up/start-puppeteer.js
+++ src/start-up/start-puppeteer.js
@@ -1,11 +1,9 @@
 const puppeteer = require('puppeteer');
 
-// Start Puppeteer and initialize global variable
 const startPuppeteer = async () => {
     try {
         console.log('Starting Puppeteer...');
 
-        // Launch Puppeteer in the new headless mode => { headless: "new" }
         global.renderPuppeteer = await puppeteer.launch({ headless: "new" });
 
         console.log('Puppeteer is online.');
