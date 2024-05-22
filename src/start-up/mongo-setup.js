11dee58f4a2b3f9863d2581b9bb2daef67e557e4
Nicusch <62447448+Nixkuchengifthub@users.noreply.github.com>
2024-05-22 21:07:27 +0200

    mongoDb await method

--- src/start-up/mongo-setup.js
+++ src/start-up/mongo-setup.js
@@ -2,13 +2,18 @@ const { MongoClient } = require('mongodb');
 
 const mongoClient = new MongoClient(process.env.MONGO_URL);
 
-mongoClient.connect()
-    .then(() => {
-        console.log('Connected to MongoDB');
-    })
-    .catch((err) => {
-        console.error('Error connecting to MongoDB:', err);
-        process.exit(1);
-    });
+const connect = () => {
+    mongoClient.connect()
+        .then(() => {
+            console.log('Connected to MongoDB');
+            return true;
+        })
+        .catch((err) => {
+            console.error('Error connecting to MongoDB:', err);
+            process.exit(1);
+        });
+}
 
 global.mongoClient = mongoClient;
+
+module.exports = { connect };
