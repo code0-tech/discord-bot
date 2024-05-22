19d86034061e2a0f27e48ef8524786a4cf497005
Nicusch <62447448+Nixkuchengifthub@users.noreply.github.com>
2024-05-22 21:09:58 +0200

    init

--- src/start-up/xp-voice-channel.js
+++ src/start-up/xp-voice-channel.js
@@ -1,47 +1,69 @@
+const { checkState } = require('./../discord/voice');
 const { Events } = require('discord.js');
+const { msToHumanReadableTime } = require('./../utils/time');
+
+let voiceChatUser = {};
 
 
+const joinVoice = (client, userid) => {
+    voiceChatUser[userid] = {
+        since: Date.now()
+    }
+}
+
+const switchVoice = (client, userid) => {
+    if (!voiceChatUser[userid]) {
+        voiceChatUser[userid] = {
+            since: client.startDate
+        }
+    }
 
 
+}
 
 
+const leaveVoice = (client, userid) => {
+    if (!voiceChatUser[userid]) {
+        voiceChatUser[userid] = {
+            since: client.startDate
+        }
+    }
 
 
+    const timeInVoice = Date.now() - voiceChatUser[userid].since;
+
+    console.log(msToHumanReadableTime(timeInVoice))
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    console.log("save db ")
+}
 
 
 
 
 const start = (client) => {
 
+    client.on(Events.VoiceStateUpdate, async (oldState, newState) => {
 
-    client.on(Events.VoiceServerUpdate, info => {
-        // delete info.guild;
-        console.log(info)
-    });
+        const { state, userid } = await checkState(oldState, newState);
 
-    client.on(Events.VoiceStateUpdate, info => {
-        // delete info.guild;
-        console.log(info)
+        console.log(state)
+
+        switch (state) {
+            case 'JOIN':
+                joinVoice(client, userid);
+                break;
+            case 'LEAVE':
+                leaveVoice(client, userid);
+                break;
+            case 'SWITCH':
+                switchVoice(client, userid);
+                break;
+
+            default:
+                break;
+        }
+
     });
 }
 
