37fb5dc6c6f4443409db4ce3e3f3002adfb9afde
Nicusch <62447448+Nixkuchengifthub@users.noreply.github.com>
2024-05-24 01:10:59 +0200

    simple setup

---
+++ src/commands/stats.js
@@ -0,0 +1,47 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { Embed, progressBar } = require('./../models/Embed');
+const { MongoUser } = require('./../mongo/MongoUser');
+const config = require('./../../config.json');
+
+const data = new SlashCommandBuilder()
+    .setName('stats')
+    .setDescription('Check you Stats.')
+    .addUserOption(option =>
+        option
+            .setName('user')
+            .setDescription('Check other user stats.')
+            .setRequired(false)
+    )
+
+
+const execute = async (interaction, client, guild, member, lang) => {
+    await interaction.deferReply({ ephemeral: true });
+
+    const userIdToCheck = interaction.options._hoistedOptions.length !== 0 ? interaction.options._hoistedOptions[0].user.id : member.user.id;
+    const rankMember = await guild.members.fetch(userIdToCheck);
+
+    const user = await new MongoUser(userIdToCheck).init();
+
+    // const { level, neededXp, xp } = await user.getRank();
+
+    // const position = await user.getXpGlobalPosition();
+
+    // let embedMessage = interaction.options._hoistedOptions.length == 0 ? 'own-rank-response' : 'other-rank-response';
+
+    // if (client.user.id == userIdToCheck) {
+    // embedMessage = 'this-bot-rank';
+    // }
+
+    /* const embed = new Embed()
+        .setColor(config.embeds.colors.info)
+        .setPbThumbnail(rankMember)
+        .addInputs({ rankuserid: userIdToCheck, level, neededXp, xp, progressbar: progressBar(xp, neededXp), position })
+        .addContext(lang, member, embedMessage); */
+
+    // embed.interactionResponse(interaction);
+};
+
+
+const componentIds = [];
+
+module.exports = { execute, componentIds, data };
