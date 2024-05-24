0c3698eb695953a930d57681d1e39f0aac46df0d
Nicusch <62447448+Nixkuchengifthub@users.noreply.github.com>
2024-05-24 17:39:24 +0200

    moved search data here

---
+++ data/search/search.js
@@ -0,0 +1,51 @@
+
+const searchData = [
+    {
+        search: "What is code0",
+        hastags: [
+            "Code0",
+            "Us",
+        ],
+        titel: "What is Code0",
+        description: ""
+    },
+    {
+        search: "What is this Discord Bot",
+        hastags: [
+            "Discord",
+            "Bot",
+        ],
+        titel: "What is Discord Bot",
+        description: ""
+    },
+    {
+        search: "What is Discord",
+        hastags: [
+            "Discord",
+            "Chat",
+        ],
+        titel: "What is Discord",
+        description: ""
+    },
+    {
+        search: "Why do people use Discord",
+        hastags: [
+            "Discord",
+            "Community",
+        ],
+        titel: "Why do people use Discord",
+        description: ""
+    },
+    {
+        search: "How to use commands",
+        hastags: [
+            "Discord",
+            "Commands",
+        ],
+        titel: "How to use commands",
+        description: ""
+    },
+    // Add more search data objects here...
+];
+
+module.exports = { searchData };
