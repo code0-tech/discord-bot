31cb305f1c161d585b1ace52757eca98e31b61e1
Nicusch <62447448+Nixkuchengifthub@users.noreply.github.com>
2024-05-24 02:06:04 +0200

    resort imports

--- src/commands/stats.js
+++ src/commands/stats.js
@@ -1,7 +1,7 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
-const { Embed } = require('./../models/Embed');
 const { msToHumanReadableTime } = require('./../utils/time');
 const { MongoUser } = require('./../mongo/MongoUser');
+const { Embed } = require('./../models/Embed');
 const config = require('./../../config.json');
 
 const data = new SlashCommandBuilder()
