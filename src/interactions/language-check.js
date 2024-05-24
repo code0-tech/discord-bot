b5fa2feb9a72f9a2eb09d44b795e912d9880ef05
Nicusch <62447448+Nixkuchengifthub@users.noreply.github.com>
2024-05-24 17:17:25 +0200

    new method of lang check

--- src/interactions/language-check.js
+++ src/interactions/language-check.js
@@ -2,25 +2,27 @@ const config = require('./../../config.json');
 
 const language = async (commandName, interaction, guild, client) => {
     const { languages } = client;
-
     const member = await guild.members.fetch(interaction.user.id);
-
     let baseLanguage = 'english';
 
+    // Determine the base language based on the user's roles
     for (const languageRoleId of Object.keys(config.languageroles)) {
         if (member.roles.cache.has(languageRoleId)) {
             baseLanguage = config.languageroles[languageRoleId];
-            break;
         }
     }
 
-    if (!languages[baseLanguage]?.[commandName]) {
-        baseLanguage = 'english';
+    // Attempt to fetch the command text in the base language
+    let commandText = languages[baseLanguage]?.[commandName] || '';
+
+    // Fallback to English if the command is not found in the base language
+    if (!commandText) {
+        commandText = languages['english']?.[commandName] || '';
     }
 
     return {
         userlang: baseLanguage,
-        text: languages[baseLanguage][commandName]
+        text: commandText
     };
 }
 
