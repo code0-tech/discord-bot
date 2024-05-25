8079c470353dae7899316e7e408084eb9e59b23b
Nicusch <62447448+Nixkuchengifthub@users.noreply.github.com>
2024-05-25 03:01:48 +0200

    new search data

--- data/search/search.js
+++ data/search/search.js
@@ -4,74 +4,127 @@ const config = require('./../../config.json');
 
 const searchData = [
     {
-        "title": "Discord Bot Commands",
+        "title": "Unlock the Power of our Discord Bot Commands",
         "hashtags": [
             "commands",
             "discord bot",
-            "discord"
+            "discord",
+            "bot commands",
+            "command list",
+            "discord server"
         ],
-        "description": "Explore a variety of commands to engage with our Discord bot and unlock its full potential."
+        "description": "Discover a multitude of commands to enhance your Discord experience and maximize the potential of our bot."
     },
     {
-        "title": "Info about Xp",
+        "title": "Explore Xp: Level Up Your Interactions",
         "hashtags": [
             "xp",
             "interactions",
             "rank",
-            "stats"
+            "stats",
+            "experience",
+            "leveling up"
         ],
-        "description": "Discover our simple XP system, where leveling up occurs as you engage with the community. Explore your rank with `/rank`, uncover the real value behind your interactions, ensuring a more accurate representation of your contributions, and compete for the top spot on the leaderboard with `/leaderboard`."
+        "description": "Dive into our straightforward XP system, where community engagement leads to leveling up. Learn about your rank with `/rank`, delve into the true value of your interactions, ensuring an accurate reflection of your contributions, and compete for the top spot on the leaderboard with `/leaderboard`."
     },
     {
-        "title": "Code0 Discord",
+        "title": "Code0's Vibrant Discord Community",
         "hashtags": [
             "discord",
             "community",
             "code0",
-            "member"
+            "member",
+            "community support",
+            "discord group"
         ],
-        "description": "Join our vibrant Discord community to find support, discover relevant content, and connect with fellow members interested in Code0. Engage in discussions, seek assistance, and be part of a welcoming community dedicated to all things Code0."
+        "description": "Become part of our lively Discord community to find support, discover relevant content, and connect with like-minded members passionate about Code0. Engage in discussions, seek assistance, and enjoy a welcoming community dedicated to all things Code0."
     },
     {
-        "title": "Be a part of our Closed Team",
+        "title": "Elevate Your Involvement: Join our Closed Team",
         "hashtags": [
             "team",
             "closed",
             "application",
-            "apply"
+            "apply",
+            "team membership",
+            "exclusive access"
         ],
-        "description": "Ready to take your involvement to the next level? Start by becoming an open contributor through `/open-contributor`, then link your GitHub account to unlock access. Once you've earned the role, head over to the <#" + config.channels.application + "> channel and submit your application to join our exclusive closed team."
+        "description": "Ready to take your involvement to the next level? Begin as an open contributor through `/open-contributor`, then link your GitHub account to unlock access. Once you've earned the role, head over to the <#" + config.channels.application + "> channel and submit your application to join our exclusive closed team."
     },
     {
-        "title": "Git Channel",
+        "title": "Stay Updated with our Git Channel",
         "hashtags": [
             "git",
             "channel",
             "discord",
-            "github"
+            "github",
+            "version control",
+            "repository"
         ],
-        "description": "The Git Channel serves as a GitHub log channel."
+        "description": "Our Git Channel serves as a log for GitHub activities."
     },
     {
-        "title": "Discord Command List?",
+        "title": "Discover Discord Commands with Ease",
         "hashtags": [
             "bot",
             "commands",
-            "discord"
+            "discord",
+            "bot interaction",
+            "command usage",
+            "discord bot guide"
         ],
-        "description": "Well we got no list but just use `/` and the command will pop up just like you executed this command."
+        "description": "No need for a list! Just use `/` followed by the command. A description will pop-up showing you all you need to know."
     },
-    { // edit on changes
-        "title": "Languages",
+    {
+        "title": "Experience Multilingual Support",
         "hashtags": [
             "lang",
             "english",
             "german",
             "spanish",
             "multilanguage",
-            "multiculti"
+            "multiculti",
+            "language support",
+            "multilingual",
+            "language roles"
         ],
-        "description": "Currently we got two languages supported.\n" + Object.entries(config.languageroles).map(([key, language]) => `- ${language}.`).join('\n') + "\nSomtimes `/`-commands or other messages are not translated to other languages like German, those messages will be displayed in English. Also if you have multiple language roles we try to use English instead."
+        "description": "Enjoy support for " + Object.entries(config.languageroles).length + " languages:\n" + Object.entries(config.languageroles).map(([key, language]) => `- ${language}.`).join('\n') + "\nSometimes `/` commands or other messages are not translated into other languages like German; in those cases, messages will be displayed in English. Also, if you have multiple language roles, we default to English."
+    },
+    {
+        "title": "Is /search Google?",
+        "hashtags": [
+            "google",
+            "search",
+            "searching",
+            "what is this",
+            "search engine",
+            "online search"
+        ],
+        "description": "The simple answer is `no`, this is not like [Google](https://www.google.com) but rather a small search for our Discord server."
+    },
+    {
+        "title": "Can you make me a Cake?",
+        "hashtags": [
+            "Cake",
+            "go",
+            "do",
+            "create",
+            "cakemaster",
+            "execute"
+        ],
+        "description": "Sorry i cant make Cakes, since i never ... did ... them?... well idk really ."
+    },
+    {
+        "title": "Large Language Models",
+        "hashtags": [
+            "language model",
+            "AI",
+            "GPT",
+            "NLP",
+            "artificial intelligence",
+            "natural language processing"
+        ],
+        "description": "ChatGpt, LLama3, ... these AI-powered systems are at the forefront of natural language processing (NLP) technology. They have been trained on vast amounts of text data to understand and generate human-like text. From answering questions to generating creative content, large language models are revolutionizing how we interact with and utilize language in various applications."
     }
 ];
 
