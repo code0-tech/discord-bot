6a593c1ce9c483a157a2e8e512fb6e6bd6c54d6b
Nicusch <62447448+Nixkuchengifthub@users.noreply.github.com>
2024-05-29 16:25:41 +0200

    modify audit logs events Embed

--- src/start-up/audit-log.js
+++ src/start-up/audit-log.js
@@ -2,6 +2,8 @@ const { Card } = require('./../models/card/Card');
 const { Embed } = require('./../models/Embed');
 const config = require('./../../config.json');
 const { Events } = require('discord.js');
+const AUDIT_LOG_ACTIONS = require('./../../data/discord/action-ids');
+const OVERWRITE_PERMISSIONS = require('./../../data/discord/permission-ids')
 
 const setup = (client) => {
     const guild = client.guilds.cache.get(process.env.GUILD_ID);
@@ -10,38 +12,39 @@ const setup = (client) => {
     let eventHand = [];
 
     const sendEvent = async (event) => {
-        /* const username = event.executor.username;
+        /* const actionId = event.action;
 
-        console.dir(event, { depth: null, breakLength: 0 });
+        console.log(event)
 
-        let changes = "";
 
-        event.changes.forEach(change => {
-            let oldValue = change.old;
-            let newValue = change.new;
+        let description = "";
 
-            // If the old or new value is an array, format it as a string
-            if (Array.isArray(oldValue)) {
-                oldValue = oldValue.join(", ");
-            }
-            if (Array.isArray(newValue)) {
-                newValue = newValue.join(", ");
-            }
+        if (actionId == 14 && event.changes) {// CHANNEL_OVERWRITE_UPDATE
+            description += event.changes.map(change => {
+                let oldValue = change.old;
+                let newValue = change.new;
 
-            changes += `\n\`${change.key}\` from \`${oldValue}\` to \`${newValue}\``;
-        });
+                if (Array.isArray(oldValue)) {
+                    oldValue = oldValue.join(', ');
+                }
+                if (Array.isArray(newValue)) {
+                    newValue = newValue.join(', ');
+                }
+
+                return `\`${change.key}\` from \`${OVERWRITE_PERMISSIONS[oldValue]}\` to \`${OVERWRITE_PERMISSIONS[newValue]}\``;
+            }).join('\n');
+        }
+
+
+
+
 
         new Embed()
             .setColor(config.embeds.colors.info)
-            .setTitle(`Target: ${event.targetType}`)
-            .setDescription(`
-        Action: ${event.actionType}
-        ${changes}
-        
-        
-        `).responseToChannel(config.channels.auditlog, client);
-        */
-
+            .setTitle(`${AUDIT_LOG_ACTIONS[actionId]} (action: ${actionId})`)
+            .setDescription(description)
+            .responseToChannel(config.channels.auditlog, client);
+ */
 
         const attachment = await new Card()
             .header({}, card => {
@@ -50,7 +53,7 @@ const setup = (client) => {
                 card.label({ value: 'Debug' }, card => { });
             })
             .body({}, card => {
-                card.description({ value: `targetType: ${event.targetType}, actionType: ${event.actionType}, action: ${event.action}` }, card => { });
+                card.description({ value: `targetType: ${event.targetType}, actionType: ${event.actionType}, action: ${AUDIT_LOG_ACTIONS[event.action]}` }, card => { });
                 card.divider({}, card => { });
                 card.description({ value: `Changes:` }, card => { });
                 card.description({ value: `${JSON.stringify(event.changes)}` }, card => { });
@@ -78,7 +81,8 @@ const setup = (client) => {
     }, 5000);
 
     client.on(Events.GuildAuditLogEntryCreate, auditLog => {
-        eventHand.push(auditLog);
+        // eventHand.push(auditLog);
+        sendEvent(auditLog);
     })
 };
 
