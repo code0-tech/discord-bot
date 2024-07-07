cba3a2036e324f00c62e655f8f522a9acb759b07
Nicusch <62447448+Nixkuchengifthub@users.noreply.github.com>
2024-07-07 21:19:52 +0200

    language for [Left Server]

--- src/commands/leaderboard.js
+++ src/commands/leaderboard.js
@@ -72,7 +72,7 @@ const execute = async (interaction, client, guild, member, lang) => {
         let leadboardMember = await DC.memberById(user.id, guild);
 
         if (leadboardMember == undefined) {
-            leadboardMember = { nickname: '[Left the server]' };
+            leadboardMember = { nickname: `[${lang.text['left-server']}]` };
         }
 
         if (user.rawxp == 0) return;
