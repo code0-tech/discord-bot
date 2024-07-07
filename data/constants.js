4c022e0ffacf76985af8232426bb5aa810b5c2a6
Nicusch <62447448+Nixkuchengifthub@users.noreply.github.com>
2024-07-07 21:57:07 +0200

    added more colors and updated way to use

--- data/constants.js
+++ data/constants.js
@@ -1,13 +1,35 @@
 class Constants {
     static EXAMPLE = "Example";
 
-
     static get DISCORD() {
         return {
             PERMS: {
                 get USER_OVERRIDE() {
                     return 1;
                 }
+            }
+        };
+    }
+
+    static get CONSOLE() {
+        return {
+            get GOOD() {
+                return '#1';
+            },
+            get WORKING() {
+                return '#2';
+            },
+            get ERROR() {
+                return '#3';
+            },
+            get LOADING() {
+                return '#4';
+            },
+            get INFO() {
+                return '#5';
+            },
+            get FOUND() {
+                return '#6';
             }
         };
     }
