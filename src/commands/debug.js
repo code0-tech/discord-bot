7387e467bfac54eb1c6e5f06eb948ebf23a2e264
Nicusch <62447448+Nixkuchengifthub@users.noreply.github.com>
2024-07-14 17:22:07 +0200

    new command wip

---
+++ src/commands/debug.js
@@ -0,0 +1,33 @@
+const { ChannelType, StringSelectMenuBuilder, StringSelectMenuOptionBuilder, PermissionFlagsBits, PermissionsBitField, ButtonBuilder, ButtonStyle, ActionRowBuilder, DiscordjsError, AttachmentBuilder } = require("discord.js");
+const { waitMs, snowflakeToDate, msToHumanReadableTime } = require('./../utils/time');
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { Embed, progressBar } = require('./../models/Embed');
+const config = require('./../../config.json');
+const DC = require('./../singleton/DC');
+
+const Chart = require('./../models/Chart');
+
+
+const data = new SlashCommandBuilder()
+    .setName('debug')
+    .setDescription('Code0 Debug commands.')
+    .setDefaultMemberPermissions(PermissionFlagsBits.Administrator)
+
+
+const execute = async (interaction, client, guild, member, lang) => {
+    await DC.defer(interaction);
+
+
+
+}
+
+
+const executeComponent = async (interaction, client, guild, buttonData, member, lang) => {
+    await DC.defer(interaction);
+
+
+}
+
+const componentIds = [];
+
+module.exports = { execute, executeComponent, data };
