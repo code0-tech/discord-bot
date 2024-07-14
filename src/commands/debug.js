eae546d18271648f56d095fd6ab916a94af5fdf4
Nicusch <62447448+Nixkuchengifthub@users.noreply.github.com>
2024-07-15 01:02:47 +0200

    more features added

--- src/commands/debug.js
+++ src/commands/debug.js
@@ -1,5 +1,5 @@
 const { ChannelType, StringSelectMenuBuilder, StringSelectMenuOptionBuilder, PermissionFlagsBits, PermissionsBitField, ButtonBuilder, ButtonStyle, ActionRowBuilder, DiscordjsError, AttachmentBuilder } = require("discord.js");
-const { waitMs, snowflakeToDate, msToHumanReadableTime } = require('./../utils/time');
+const { msToHumanReadableTime, convertUnixToTimestamp, waitMs } = require('./../utils/time');
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const { Embed, progressBar } = require('./../models/Embed');
 const { Mongo, ENUMS } = require('../models/Mongo');
@@ -18,27 +18,77 @@ const data = new SlashCommandBuilder()
             .setRequired(true)
             .addChoices(
                 { name: 'Mongo => Check left Users', value: 'mongoLeftUsers' },
+                { name: 'Client => This session time', value: 'clientSessionTime' }
             ))
     .setDefaultMemberPermissions(PermissionFlagsBits.Administrator)
 
-// show bar of server user / left user in procentage
 
-
-
-
-const debugs = {
-    mongoLeftUsers(interaction, client, guild, member, lang) {
-
-    }
-
-
-
-
-
+const info = (title, des) => {
 
 }
 
+const debugs = {
+    async mongoLeftUsers(interaction, client, guild, member, lang) {
+        new Embed()
+            .setColor(config.embeds.colors.info)
+            .addContext(lang, member, 'mongo-left-users-loading')
+            .interactionResponse(interaction);
 
+        const pipeline = [
+            {
+                $project: {
+                    id: 1,
+                    _id: 1
+                }
+            }
+        ]
+
+        const userIds = await MongoDb.aggregate(ENUMS.DCB.USERS, pipeline);
+
+        let usersLeft = [];
+
+        for (let i = 0; i < userIds.length; i++) {
+            const userPacket = userIds[i];
+            const result = await DC.memberById(userPacket.id, guild);
+            if (!result) {
+                usersLeft.push(userPacket.id);
+            }
+        }
+
+        new Embed()
+            .setColor(config.embeds.colors.info)
+            .addInputs({
+                userstring: (usersLeft.length == 0 ? `${lang.getText("found-none")}` : usersLeft.join("\n")),
+                progressbar: progressBar(usersLeft.length, userIds.length) + ` [${usersLeft.length}|${userIds.length}]`
+            })
+            .addContext(lang, member, 'mongo-left-users')
+            .interactionResponse(interaction);
+
+        return;
+    },
+
+    async clientSessionTime(interaction, client, guild, member, lang) {
+        const { d, h, m, s } = msToHumanReadableTime(Date.now() - client.startDate);
+
+        const embed = new Embed()
+            .setColor(config.embeds.colors.info)
+            .addInputs({
+                upsince: convertUnixToTimestamp(client.startDate),
+                days: d,
+                hours: h,
+                minutes: m,
+                seconds: s
+            })
+            .addContext(lang, member, 'client-session-time');
+
+        const response = await embed.interactionResponse(interaction);
+        if (response == null) return;
+
+        await waitMs(5000);
+
+        this.clientSessionTime(interaction, client, guild, member, lang);
+    }
+}
 
 
 const execute = async (interaction, client, guild, member, lang) => {
@@ -46,7 +96,7 @@ const execute = async (interaction, client, guild, member, lang) => {
 
     const type = interaction.options.getString('type');
 
-    debugs[type](interaction, client, guild, member, lang);
+    await debugs[type](interaction, client, guild, member, lang);
 }
 
 
