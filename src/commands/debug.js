c47a8210bebe65d39ba009462b011152ebf32696
Nicusch <62447448+Nixkuchengifthub@users.noreply.github.com>
2024-07-15 11:17:30 +0200

    new command for creating a chart out of the githhubcommits

--- src/commands/debug.js
+++ src/commands/debug.js
@@ -6,8 +6,8 @@ const { Mongo, ENUMS } = require('../models/Mongo');
 const config = require('./../../config.json');
 const Chart = require('./../models/Chart');
 const DC = require('./../singleton/DC');
-const MongoDb = new Mongo();
 
+const MongoDb = new Mongo();
 
 const data = new SlashCommandBuilder()
     .setName('debug')
@@ -18,7 +18,9 @@ const data = new SlashCommandBuilder()
             .setRequired(true)
             .addChoices(
                 { name: 'Client => This session time', value: 'clientSessionTime' },
-                { name: 'Mongo => Check left Users', value: 'mongoLeftUsers' }
+                { name: 'Mongo => Check left Users', value: 'mongoLeftUsers' },
+                { name: 'Mongo => githubcommits -> new Chart', value: 'githubTotalCommits' }
+
             ))
     .setDefaultMemberPermissions(PermissionFlagsBits.Administrator)
 
@@ -59,8 +61,6 @@ const debugs = {
             })
             .addContext(lang, member, 'mongo-left-users')
             .interactionResponse(interaction);
-
-        return;
     },
 
     async clientSessionTime(interaction, client, guild, member, lang) {
@@ -83,6 +83,108 @@ const debugs = {
         await waitMs(5000);
 
         this.clientSessionTime(interaction, client, guild, member, lang);
+    },
+
+    async githubTotalCommits(interaction, client, guild, member, lang) {
+        const getRandomColor = () => {
+            const r = Math.floor(Math.random() * 255);
+            const g = Math.floor(Math.random() * 255);
+            const b = Math.floor(Math.random() * 255);
+            return `rgb(${r},${g},${b})`;
+        }
+
+        const getNextDate = (dateString) => {
+            const date = new Date(dateString);
+            date.setDate(date.getDate() + 1);
+            return date.toISOString().slice(0, 10);
+        }
+
+        const uniqIds = await MongoDb.distinct(ENUMS.DCB.GITHUB_COMMITS, "name");
+
+        const pipeline = [
+            {
+                $sort: { time: 1 }
+            },
+            {
+                $group: {
+                    _id: {
+                        name: "$name",
+                        date: {
+                            $dateToString: { format: "%Y-%m-%d", date: { $toDate: "$time" } }
+                        }
+                    },
+                    dailyCommits: { $sum: "$commitscount" }
+                }
+            },
+            {
+                $sort: { "_id.date": 1 }
+            }
+        ];
+
+        const cursor = await MongoDb.aggregate(ENUMS.DCB.GITHUB_COMMITS, pipeline);
+        const dbEntries = await cursor;
+
+        const firstDate = dbEntries[0]._id.date;
+        const lastDate = new Date().toISOString().slice(0, 10);
+
+        const cumulativeCommits = {};
+
+        dbEntries.forEach(entry => {
+            const { name, date } = entry._id;
+            const dailyCommits = entry.dailyCommits;
+
+            if (!cumulativeCommits[name]) {
+                cumulativeCommits[name] = [];
+            }
+
+            cumulativeCommits[name].push({ date, commits: dailyCommits });
+        });
+
+        for (const name in cumulativeCommits) {
+            const userData = cumulativeCommits[name];
+            const allDates = userData.map(entry => entry.date);
+
+            const filledData = [];
+            let currentDate = firstDate;
+            let currentIndex = 0;
+            let currentCumulative = 0;
+
+            while (currentDate <= lastDate) {
+                if (currentIndex < userData.length && allDates[currentIndex] === currentDate) {
+                    currentCumulative += userData[currentIndex].commits;
+                    filledData.push({ date: currentDate, commits: currentCumulative });
+                    currentIndex++;
+                } else {
+                    filledData.push({ date: currentDate, commits: currentCumulative });
+                }
+
+                currentDate = getNextDate(currentDate);
+            }
+
+            cumulativeCommits[name] = filledData;
+        }
+
+        const labels = Object.values(cumulativeCommits).flatMap(user => user.map(entry => entry.date)).filter((value, index, self) => self.indexOf(value) === index);
+        const datasets = [];
+
+        for (const [name, data] of Object.entries(cumulativeCommits)) {
+            datasets.push({
+                label: name,
+                data: data.map(entry => entry.commits),
+                borderColor: getRandomColor(),
+                fill: false
+            });
+        }
+
+        const chart = new Chart(1000, 600)
+            .setType('line')
+            .setLabels(labels);
+
+        datasets.forEach(dataset => {
+            chart.addDataset(dataset.label, dataset.data, dataset.borderColor);
+        });
+
+        chart.interactionResponse(interaction);
     }
 }
 
@@ -92,7 +194,7 @@ const execute = async (interaction, client, guild, member, lang) => {
 
     const type = interaction.options.getString('action');
 
-    await debugs[type](interaction, client, guild, member, lang);
+    debugs[type](interaction, client, guild, member, lang);
 }
 
 
