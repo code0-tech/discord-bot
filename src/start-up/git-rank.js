4d595893ddf66533a1cc411fa0e748efacc98307
Nicusch <62447448+Nixkuchengifthub@users.noreply.github.com>
2024-07-18 10:26:19 +0200

    refractored code and added a leaderboard table using SimpleTable + added the days ago string for the table showing how many days this message counts in total

--- src/start-up/git-rank.js
+++ src/start-up/git-rank.js
@@ -1,35 +1,38 @@
-const { convertUnixToTimestampSmall } = require('../utils/time');
-const { Embed, progressBar } = require('../models/Embed');
 const { SimpleTable } = require('../models/SimpleTable');
 const GITCOMMITS = require('./../singleton/GITCOMMITS');
 const { Mongo, ENUMS } = require('../models/Mongo');
 const Constants = require('../../data/constants');
 const config = require('./../../config.json');
+const { Embed } = require('../models/Embed');
 const schedule = require('node-schedule');
-
 
 const MongoDb = new Mongo();
 
-const placeMedals = [
-    Constants.DISCORD.EMOJIS.FIRSTPLACE,
-    Constants.DISCORD.EMOJIS.SECONDPLACE,
-    Constants.DISCORD.EMOJIS.THIRDPLACE
-];
+const totalDays = async () => {
+    const result = await MongoDb.aggregate(ENUMS.DCB.GITHUB_COMMITS, [
+        { $group: { _id: null, minTime: { $min: '$time' } } }
+    ])
 
-const sendGitRankMessage = async (client) => {
-    const spanEndDate = Date.now();
-    const spanStartDate = spanEndDate - 24 * 60 * 60 * 1000;
+    const time = result.length > 0 ? result[0].minTime : null;
 
+    const now = Date.now();
+    const differenceInMillis = now - time;
+    const days = Math.floor(differenceInMillis / (1000 * 60 * 60 * 24));
+    return `${days}`;
+}
+
+const fetchGitCommits = async (spanStartDate, spanEndDate) => {
     const query = {
         time: {
             $gte: spanStartDate,
             $lte: spanEndDate
         }
     };
+    return await MongoDb.find(ENUMS.DCB.GITHUB_COMMITS, query);
+}
 
-    const documents = await MongoDb.find(ENUMS.DCB.GITHUB_COMMITS, query);
-
-    const userStats = documents.reduce((acc, doc) => {
+const calculateUserStats = (documents) => {
+    return documents.reduce((acc, doc) => {
         const { name, repo, branchname, commitscount } = doc;
         if (!acc[name]) {
             acc[name] = { total: 0, repos: {} };
@@ -41,43 +44,50 @@ const sendGitRankMessage = async (client) => {
         acc[name].repos[repo].add(branchname);
         return acc;
     }, {});
+}
 
-    const formattedUserStats = Object.entries(userStats).map(([name, { total, repos }]) => ({
+const formatUserStats = (userStats) => {
+    return Object.entries(userStats).map(([name, { total, repos }]) => ({
         name,
         total,
         repos: Object.entries(repos).map(([repo, branches]) => ({
             repo,
             branches: Array.from(branches)
         }))
+    })).sort((a, b) => b.total - a.total);
+}
+
+const fetchAllCommitsCounts = async (names) => {
+    const results = await MongoDb.aggregate(ENUMS.DCB.GITHUB_COMMITS, [
+        { $match: { name: { $in: names } } },
+        { $group: { _id: '$name', totalCommits: { $sum: '$commitscount' } } }
+    ]);
+
+    return results.reduce((acc, { _id, totalCommits }) => {
+        acc[_id] = totalCommits;
+        return acc;
+    }, {});
+}
+
+const updatePackets = (formattedUserStats, commitsCounts) => {
+    return formattedUserStats.map(packet => ({
+        name: packet.name,
+        total: packet.total,
+        alldaystotal: commitsCounts[packet.name]
     }));
+}
 
-    formattedUserStats.sort((a, b) => b.total - a.total);
-
+const buildLeaderboardDescription = async (formattedUserStats, updatedPackets) => {
     let description = `
 ### ${Constants.DISCORD.EMOJIS.TROPHY} Winner: ${formattedUserStats[0].name} ${Constants.DISCORD.EMOJIS.TROPHY}
 
 Commits: \`${formattedUserStats[0].total}\` in the last 24 hours.
 ### Commits Leaderboard\n`;
 
-    let updatedPackets = [];
-
-    for (const packet of formattedUserStats) {
-        const alldaystotal = (await MongoDb.aggregate(ENUMS.DCB.GITHUB_COMMITS, [
-            { $match: { name: packet.name } },
-            { $group: { _id: '$name', totalCommits: { $sum: '$commitscount' } } }
-        ]))[0].totalCommits;
-        const updatedPacket = {
-            name: packet.name,
-            total: packet.total,
-            alldaystotal
-        };
-        updatedPackets.push(updatedPacket);
-    }
-
     const columns = [
         { label: 'User', key: 'name' },
-        { label: 'Total', key: 'alldaystotal' },
-        { label: 'last 24h', key: 'total' }
+        { label: `Last ${await totalDays()} days`, key: 'alldaystotal' },
+        { label: `Last 24h`, key: 'total' }
     ];
 
     const tableString = await new SimpleTable(columns)
@@ -88,15 +98,32 @@ Commits: \`${formattedUserStats[0].total}\` in the last 24 hours.
         .build();
 
     description += tableString;
+    return description;
+}
 
-    const embed = new Embed()
+const createEmbedMessage = async (description) => {
+    return new Embed()
         .setColor(config.embeds.colors.info)
         .setDescription(description)
         .setAttachment(await GITCOMMITS.getAttachment())
-        .setImage(`attachment://chart.png`)
+        .setImage(`attachment://chart.png`);
+}
 
-    if (client == null) { // for debug only
-        return embed;
+const sendGitRankMessage = async (client) => {
+    const spanEndDate = Date.now();
+    const spanStartDate = spanEndDate - 24 * 60 * 60 * 1000;
+
+    const documents = await fetchGitCommits(spanStartDate, spanEndDate);
+    const userStats = calculateUserStats(documents);
+    const formattedUserStats = formatUserStats(userStats);
+    const names = formattedUserStats.map(packet => packet.name);
+    const commitsCounts = await fetchAllCommitsCounts(names);
+    const updatedPackets = updatePackets(formattedUserStats, commitsCounts);
+    const description = await buildLeaderboardDescription(formattedUserStats, updatedPackets);
+    const embed = await createEmbedMessage(description);
+
+    if (client == null) {
+        return embed; // for debug only
     }
 
     embed.responseToChannel(config.channels.gitranks, client);
