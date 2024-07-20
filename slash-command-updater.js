b7b66ab019a8d9ee174e3355318b18db3207af51
Nicusch <62447448+Nixkuchengifthub@users.noreply.github.com>
2024-07-20 22:37:50 +0200

    comments added for better understanding

--- slash-command-updater.js
+++ slash-command-updater.js
@@ -9,7 +9,7 @@ const commandFiles = fs.readdirSync(path.join(__dirname, 'src', 'commands')).fil
 
 commandFiles.forEach(file => {
     const command = require(path.join(__dirname, 'src', 'commands', file));
-    if (command.data !== null) {
+    if (command.data !== null) { // no command data aka "_"-command.
         commands.push(command.data.toJSON());
         console.log(`Register: ${command.data.name}`);
     }
@@ -20,6 +20,6 @@ const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
 rest.put(Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
     { body: commands })
     .then(() => {
-        console.log("Registered Commands!")
+        console.log("Registered all Commands!")
     })
     .catch(console.error)
