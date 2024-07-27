7615ac955f73391993f7c7272339c6cdfd0ff090
Nicusch <62447448+Nixkuchengifthub@users.noreply.github.com>
2024-07-27 15:41:50 +0200

    new command

---
+++ src/commands/mydata.js
@@ -0,0 +1,33 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MongoUser } = require('./../mongo/MongoUser');
+const { AttachmentBuilder } = require('discord.js');
+const { Embed } = require('./../models/Embed');
+const config = require('./../../config.json');
+const DC = require('./../singleton/DC');
+
+const data = new SlashCommandBuilder()
+    .setName('mydata')
+    .setDescription('Get your Data which is stored in our DB.')
+    .setDescriptionLocalizations({
+        de: 'Erhalte deine Daten die wir über Dich gespeichert haben.',
+    })
+
+const execute = async (interaction, client, guild, member, lang) => {
+    await DC.defer(interaction);
+
+    const mongoUser = new MongoUser(member.user.id);
+
+    const jsonString = JSON.stringify(await mongoUser.getJson(), null, 2);
+
+    const buffer = Buffer.from(jsonString, 'utf-8');
+    const attachment = new AttachmentBuilder(buffer, { name: 'sample.json' });
+
+    new Embed()
+        .setColor(config.embeds.colors.info)
+        .addContext(lang, member, 'your-data')
+        .setAttachment(attachment)
+        .interactionResponse(interaction);
+};
+
+
+module.exports = { execute, data };
