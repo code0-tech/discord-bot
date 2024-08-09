e18f27342309f7af0d99c0c96fd522966be1cab4
Nicusch <62447448+Nixkuchengifthub@users.noreply.github.com>
2024-08-09 20:22:44 +0200

    Wip

---
+++ src/models/AsyncManager.js
@@ -0,0 +1,15 @@
+class AsyncManager {
+    constructor() {
+        this.actions = {};
+    }
+
+    generateId() {
+        const timestamp = Date.now();
+        const random = Math.floor(Math.random() * 1_000_000);
+        return `${timestamp}-${random}`;
+    }
+
+    // add the rest of the functions
+}
+
+module.exports = AsyncManager;
