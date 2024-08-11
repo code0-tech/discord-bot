f7520cb096ce1eccea5de7365356e35b22d01ed5
Nicusch <62447448+Nixkuchengifthub@users.noreply.github.com>
2024-08-11 23:13:50 +0200

    git command setup

--- src/commands/git.js
+++ src/commands/git.js
@@ -1,6 +1,4 @@
-const { ButtonBuilder, ButtonStyle, ActionRowBuilder, StringSelectMenuBuilder, PermissionFlagsBits } = require("discord.js");
 const { SlashCommandBuilder } = require('@discordjs/builders');
-const { convertUnixToTimestamp } = require('../utils/time');
 const { Mongo, ENUMS } = require('../models/Mongo');
 const DiscordSimpleTable = require('discord-simpletable');
 const { Embed } = require('./../models/Embed');
@@ -13,11 +11,47 @@ const data = new SlashCommandBuilder()
     .setDescriptionLocalizations({
         de: 'Git aktivität für Code0.',
     })
+    .addSubcommand(subcommand => subcommand
+        .setName('repoPie')
+        .setDescription('Display Git repo activity as an pie chart.')
+        .setDescriptionLocalizations({
+            de: 'Zeigt Git aktivität für alle Repos als Kuchen-diagram.',
+        })
+    )
+    .addSubcommand(subcommand => subcommand
+        .setName('repoGraph')
+        .setDescription('Display Git repo activity as an graph chart.')
+        .setDescriptionLocalizations({
+            de: 'Zeigt Git aktivität für alle Repos als Graph.',
+        })
+    )
+    .addSubcommand(subcommand => subcommand
+        .setName('repoTable')
+        .setDescription('Display Git repo activity as an table.')
+        .setDescriptionLocalizations({
+            de: 'Zeigt Git aktivität für alle Repos als Tabelle.',
+        })
+    )
+
+const commands = {
+    async repoPie(interaction, client, guild, member, lang) {
+
+    },
+    async repoGraph(interaction, client, guild, member, lang) {
+
+    }
+}
 
 
 const execute = async (interaction, client, guild, member, lang) => {
     await DC.defer(interaction);
 
+    const subCommand = interaction.options.getSubcommand();
+
+    if (commands[subCommand]) {
+        commands[subCommand](interaction, client, guild, member, lang);
+    }
 }
+
 
 module.exports = { execute, data };
