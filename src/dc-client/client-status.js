66915abaf983e897c862c144e01ea53f45127fd0
Nicusch <nicolas260504@gmail.com>
2025-01-02 15:21:35 +0100

    differ status on development

--- src/dc-client/client-status.js
+++ src/dc-client/client-status.js
@@ -2,10 +2,11 @@ const config = require('../../config.json');
 
 const start = (client) => {
     const messages = config.status.messages;
+    const developmentMessages = config.status.ondevelopment;
     let index = 0;
 
     setInterval(() => {
-        const { name, status = 'online' } = messages[index++ % messages.length];
+        const { name, status = 'online' } = global.isDevelopment ? developmentMessages[index++ % developmentMessages.length] : messages[index++ % messages.length];
 
         client.user.setPresence({ activities: [{ name }], status });
     }, config.status.interval);
