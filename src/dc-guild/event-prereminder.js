ef99567fb6b56827fda920c3133337bcc6db3687
Nicusch <nicolas260504@gmail.com>
2025-02-08 14:56:50 +0100

    updated to use an config

--- src/dc-guild/event-prereminder.js
+++ src/dc-guild/event-prereminder.js
@@ -1,7 +1,7 @@
-const Constants = require("../../data/constants");
+/* const Constants = require("../../data/constants");
 const { guildById } = require("../singleton/DC");
 const config = require('../../config.json');
-const { Embed } = require("../models/Embed");
+const { Embed, replacePlaceHolders } = require("../models/Embed");
 
 const fetchUpcomingEvents = async (guild) => {
     return await guild.scheduledEvents.fetch();
@@ -47,12 +47,74 @@ const setupEventMessages = async (client) => {
 // push this later into config like this
 const configForPreEvent = [{
     title: "title",
-    mention: "user or role", // maybe do something to prevent double ids in config
+    reminderbeforeinminutes: [60, 5],
     embed: {
+        mention: "user or role",
         title: "Its time for {title} in {minutesRemaining}",
         description: "Guys lets go this time"
     }
 }]
+
+
+module.exports = { setupEventMessages }; */
+
+const Constants = require("../../data/constants");
+const { guildById } = require("../singleton/DC");
+const config = require('../../config.json');
+const { Embed, replacePlaceHolders } = require("../models/Embed");
+
+const fetchUpcomingEvents = async (guild) => {
+    return await guild.scheduledEvents.fetch();
+}
+
+const setupTimer = (channelId, title, time, client, eventConfig) => {
+    eventConfig.reminderbeforeinminutes.forEach(timeInMinutes => {
+        const timeNew = time - (timeInMinutes * 1000 * 60);
+
+        if (timeNew > 0) {
+            setTimeout(() => {
+                console.log(`[PREREMINDER-EVENTS] Time remaining ${timeInMinutes} min for "${title}"`, Constants.CONSOLE.INFO);
+
+                const placeholders = { title, minutesRemaining: timeInMinutes };
+
+                const embedMessage = new Embed()
+                    .setTitle(replacePlaceHolders(eventConfig.embed.title, placeholders))
+                    .setContent(`<@&${config.roles[eventConfig.embed.mention]}>`)
+                    .setDescription(replacePlaceHolders(eventConfig.embed.description, placeholders))
+                    .setColor(config.embeds.colors.info);
+
+                embedMessage.responseToChannel(channelId, client);
+            }, timeNew);
+        } else {
+            console.log(`[PREREMINDER-EVENTS] "${title}" invalid timeout: ${timeNew}, on call ${timeInMinutes} min before event`, Constants.CONSOLE.ERROR);
+        }
+    });
+}
+
+const setupEventMessages = async (client) => {
+    const guild = await guildById(config.serverid, client);
+    const events = await fetchUpcomingEvents(guild);
+
+    events.forEach(async (event) => {
+        const nextDateUnix = event.scheduledStartTimestamp;
+        const readableDate = new Date(nextDateUnix).toLocaleString();
+
+        console.log(`[PREREMINDER-EVENTS] "${event.name}" next occurrence (Readable): ${readableDate}`, Constants.CONSOLE.INFO);
+
+        const timeRemaining = nextDateUnix - Date.now();
+        const channelId = config.channels.internalcommunicationchat;
+
+        const eventConfig = config.commands.eventprereminder.find(cfg => cfg.title == event.name);
+
+        if (eventConfig) {
+            console.log(`[PREREMINDER-EVENTS] config found for "${event.name}"`, Constants.CONSOLE.GOOD);
+
+            setupTimer(channelId, event.name, timeRemaining, client, eventConfig);
+        } else {
+            console.log(`[PREREMINDER-EVENTS] No config found for "${event.name}", skipping...`, Constants.CONSOLE.INFO);
+        }
+    });
+}
 
 
 module.exports = { setupEventMessages };
