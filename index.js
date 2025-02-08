8ceb045c78c01699f77b5f7b5561df371a98d56f
Nicusch <nicolas260504@gmail.com>
2025-02-09 00:27:40 +0100

    removed testing

--- index.js
+++ index.js
@@ -41,8 +41,8 @@ const start = async (args) => {
     require('./src/dc-client/language-file-check');
     await require('./src/start-up/mongo-setup').connect();
 
-    require('./src/singleton/GIT');
-    return;
+    // require('./src/singleton/GIT');
+    // return;
 
     require('./src/start-up/start-puppeteer');
     require('./src/web-server/http-server').setup(client);
